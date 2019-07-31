cb347b32cded90162c8da1ee33376cf0303deb98
revSoul12 <daniel.laserna@gmail.com>
2019-07-31 20:34:41 +0000

    Implemented movement commands

--- input.js
+++ input.js
@@ -2,24 +2,40 @@
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
 
-// process.stdin.on('data', handleUserInput());
+let connection;
 
- const setupInput = function() {
+ const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
+
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  stdin.on('data', handleUserInput());
+  stdin.on('data', handleUserInput);
   
   return stdin;
 }
 
-setupInput();
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
+}
+
+
 
 module.exports = {setupInput};
